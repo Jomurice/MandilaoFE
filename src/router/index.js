f49781a6871785efbecdcf583e0eaa63e27b441b
anhkiet0802 <dinhanhkiet0802@gmail.com>
2025-07-14 11:43:48 +0700

    Commit01: header

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,15 @@ import { createRouter, createWebHistory } from "vue-router";
 import login from "../components/login/login.vue";
 import adminLogin from "../components/login/adminLogin.vue";
 import register from "../components/account/register.vue";
+import HomeUser from '../components/home/HomeUser.vue';
+import HomeAdmin from '../components/home/HomeAdmin.vue';
 
 const routes =[
     {path:"/login", component: login},
     {path:"/adminLogin", component: adminLogin},
-    {path:"/register", component: register}
+    {path:"/register", component: register},
+    { path: '/', name: 'Home', component: HomeUser },
+    { path: '/Admin', name: 'Admin', component: HomeAdmin }, 
    
 ]
 
