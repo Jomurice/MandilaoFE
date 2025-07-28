9c656785f683965b9a04fb0136e80e2b937b1845
Quang <minhquangru@gmail.com>
2025-07-28 21:01:48 +0700

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,10 @@ import eventPage from "../components/eventPage/eventPage.vue";
 import eventDetail from "../components/eventPage/eventDetail.vue";
 import profileUser from "../components/account/profile-user.vue";
 import menu from "../components/menu/menu.vue";
-<<<<<<< HEAD
 import cart from "../components/cart/cart.vue"
-=======
 import productSearch from "../components/menu/product-search.vue";
 import { compile } from "vue";
->>>>>>> feat_upgradeProduct
+
 const routes = [
   { path: "/", name: "Home", component: HomeUser },
   { path: "/Admin", name: "Admin", component: HomeAdmin },
@@ -25,15 +23,9 @@ const routes = [
   { path: "/eventDetail", component: eventDetail },
   { path: "/profileuser", component: profileUser },
   { path: "/menu", component: menu },
-<<<<<<< HEAD
-  {path: "/cart", component: cart}
-=======
-  {
-  path: '/search',
-  name: 'product-search',
-  component: productSearch
-}
->>>>>>> feat_upgradeProduct
+  {path: "/cart", component: cart},
+{path: '/search',name: 'product-search',component: productSearch}
+
 ];
 
 const router = createRouter({
